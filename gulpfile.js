020e2af007790818e37021ee7f1f13833fbfc0ff
Chase Moskal <chasemoskal@gmail.com>
2015-03-21 13:33:55 -0600

    Moved Notery from node_modules directly into gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,121 @@
+
+//==============================================================================
+//:::: NOTERY ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//==============================================================================
+//==============================================================================
+
+var Notery = (function(){
+	var chalk = require('chalk');
+
+
+	//////// STAMP /////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+
+	var Stamp = (function(){
+		function Stamp () {
+			if (!(this instanceof Stamp)) return (new Stamp).toString();
+			var d = new Date;
+			this.hours   = Stamp.lz(d.getHours(),   2);
+			this.minutes = Stamp.lz(d.getMinutes(), 2);
+			this.seconds = Stamp.lz(d.getSeconds(), 2);
+		}
+
+		Stamp.lz = function leadingZeros (v, n) {
+			v += '';
+			n -= v.length;
+
+			var o = '';
+			while (o.length < n)
+				o += '0';
+
+			return o + v;
+		};
+
+		Stamp.prototype.toString = function stampToString () {
+			return '[' +
+				this.hours +
+				 ':' +
+				this.minutes +
+				 ':' +
+				this.seconds +
+			']';
+		};
+
+		return Stamp;
+	})();
+
+
+	//////// NOTERY CONSTRUCTOR ////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+
+	function Notery (label, styles) {
+		this.label  = label  || '';
+		this.styles = styles || {};
+		this.note = Notery.prototype.note.bind(this);
+	}
+
+
+	//////// NOTERY INSTANCE METHODS ///////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+
+	Notery.prototype.style = function style (a) {
+		if (!a) throw new Error;
+
+		if (a.constructor == Object) {
+			for (var key in a)
+				this.styles[key] = a[key];
+		}
+
+		else if (a.constructor == String) { 
+			return (a in this.styles) 
+				? this.styles[a]
+				: null;
+		}
+
+		return this;
+	};
+
+
+	Notery.prototype.stylize = function stylize (style, x) {
+		var s = this.style(style);
+		return s ? s(x) : x;
+	};
+
+
+	Notery.prototype.note = function note () {
+		var notery = this;
+		var o = [];
+		var s;
+
+		o.push(notery.stylize('stamp', (new Stamp).toString()));
+
+		if (notery.label) o.push(
+			notery.stylize('label', '[' + notery.label + ']')
+		);
+
+		Array.prototype.forEach.call(arguments, function(arg){
+			o.push( notery.stylize('payload', arg) );
+		});
+
+		console.log();
+		console.log.apply(notery, o);
+		console.log();
+
+		return this;
+	};
+
+
+	//////// EXPORTATION ///////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+
+	Notery.chalk = chalk;
+	Notery.Stamp = Stamp;
+	return Notery;
+})();
+
+
+
+
 /*//////////////////////////////////////////////////////////////////////////////
 ================================================================================
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
